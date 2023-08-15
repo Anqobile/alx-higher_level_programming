8b3af4f41b39deef2cbfa5f2d69a1928d74d21c8
Anqobile <Anqobilegylar@gmail.com>
2023-08-15 12:05:32 -0700

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,6 +4,3 @@ def print_list_integer(my_list=[]):
    "print the list of all integers"
    for y in my_list:
        print("{:d}".format(y))
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)
