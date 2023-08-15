3e36544d9d35bea723eb7d5eda603e9948309880
Anqobile <Anqobilegylar@gmail.com>
2023-08-15 12:36:58 -0700

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,9 +5,3 @@ def replace_in_list(my_list, idx, element):
         return my_list
     my_list[idx] = element
     return my_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 2
-element = 1 
-
-print("{}".format(replace_in_list(my_list, idx, element)))
